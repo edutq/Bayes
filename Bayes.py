a0fb2deaed2d3cbba5eb7bd94c864be2ac36aade
Edu <etavarezq@gmail.com>
2018-03-08 17:48:42 -0600

    parsing nodes done

--- Bayes.py
+++ Bayes.py
@@ -1,14 +1,26 @@
 import itertools
 import copy
 
-class node:
+class Node:
+
 	def __init__(self, name, children, parent, table):
 		self.name = name
 		self.children = children
 		self.parent = parent
 		self.table = table
 
+	def __repr__(self):
+		return "Node:\n\tName: %s \n\tChildren: %s \n\tParents: %s \n\tTable: %s\n" % (self.name, self.children, self.parent, self.table)
+
+def create_nodes(nodes_string):
+	nodes_list = []
+	for var in nodes_string.split(','):
+		nodes_list.append(Node(var, None, None, None))
+
+	return nodes_list
+
 def bayes(nodes, probabilities, queries):
+	nodes_list = create_nodes(nodes)
 	return 0
 
 
