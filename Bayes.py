cf12f4af332eb1860372c4784d52e5728a08e702
Edu <etavarezq@gmail.com>
2018-03-15 16:07:26 -0600

    create list with all possible combinations of hidden and given variables

--- Bayes.py
+++ Bayes.py
@@ -2,6 +2,7 @@ import itertools
 import copy
 import json
 import decimal
+import math
 
 class Node:
 
@@ -84,7 +85,7 @@ def parse_probabilities(probabilities_list):
 		
 	return 	statement_list
 
-def parse_query(query): 
+def parse_query(query, node_list): 
 	#this method receives the probability to be calculated and converts it to the conditional probability form
 	numerator = ""
 	denominator = ""
@@ -99,14 +100,70 @@ def parse_query(query):
 		elif len(var) == 1:
 			numerator = var[0]
 		
-		print(numerator)
-		print(denominator)
+		conditional_probability(numerator, denominator, node_list)
 	return 0
 
-def conditional_probability(parsed_query):
-	numerator = 0
-	denominator = 0
+def conditional_probability(numerator, denominator, node_list):
+
 	#get the numerator / denominator from the parsed query
+	print()
+	print("numerator")
+	
+	numeratorhidden = []
+	numeratorgiven = numerator.split(',')
+	print(numeratorgiven)
+	for element in numeratorgiven:
+
+		if "+" in element:
+			auxelement = element.replace('+', "")
+		elif "-" in element:
+			auxelement = element.replace('-', "")
+		node = get_node(auxelement, node_list)
+		ancestors = []
+		if node.parents:
+			get_ancestors(node, node_list, ancestors)
+
+		for ancestor in ancestors:
+
+			if not "+" + ancestor in numeratorgiven and not "-" + ancestor in numeratorgiven and not ancestor in numeratorhidden:
+				numeratorhidden.append(ancestor)
+	print(numeratorhidden)
+	if numeratorhidden:
+		permitationsnumerator = create_permutations(numeratorhidden)
+		enumeratorNUvariables = appendgivenandhidden(numeratorgiven, permitationsnumerator)
+		print(enumeratorNUvariables)
+	else:
+		enumeratorNUvariables = numeratorgiven
+	print()
+	print("denominator")
+	
+	denominatorhidden = []
+	denominatorgiven = denominator.split(',')
+	print(denominatorgiven)
+	for element in denominatorgiven:
+
+		if "+" in element:
+			auxelement = element.replace('+', "")
+		elif "-" in element:
+			auxelement = element.replace('-', "")
+		node = get_node(auxelement, node_list)
+		ancestors = []
+		if node.parents:
+			get_ancestors(node, node_list, ancestors)
+
+		for ancestor in ancestors:
+
+			if not "+" + ancestor in denominatorgiven and not "-" + ancestor in denominatorgiven and not ancestor in denominatorhidden:
+				denominatorhidden.append(ancestor)
+	print(denominatorhidden)
+
+	if denominatorhidden:
+		permitationsdenominator = create_permutations(denominatorhidden)
+		#print(denominatorgiven)
+		enumeratorDEvariables = appendgivenandhidden(denominatorgiven, permitationsdenominator)
+		print(enumeratorDEvariables)
+	else:
+		enumeratorDEvariables = denominatorgiven
 	#check all the nodes that are given
 	#not given nodes have to be calculated by true and false 
 	#Apply all the permutations possible (call create_permutations method)
@@ -118,12 +175,89 @@ def conditional_probability(parsed_query):
 	# Add up all permutations
 	# Do the same for the denominator 
 	#divide 
-	return numerator / denominator
+	return 0
 
-def create_permutations():
-	permutations = []
+def get_node(name, node_list):
+	for element in node_list:
+		if element.name == name:
+			return element
+	return False
 
-	return permutations
+def appendgivenandhidden(given, combinations):
+	result = []
+	aux = []
+	for i in range(len(combinations)):
+		aux = []
+		for element in given:
+
+			aux.append(element)
+		for element in combinations[i]:
+			aux.append(element)
+		result.append(aux)
+	return result
+def get_ancestors(node, node_list, ancestors):
+	if node.parents:
+		for element in node.parents:
+			if element not in ancestors:
+				ancestors.append(element)
+			newnode = get_node(element, node_list)
+			get_ancestors(newnode, node_list, ancestors)
+	else:
+		if node.name not in ancestors:
+			ancestors.append(node.name)
+
+
+def create_permutations(hiddenvariables):
+	combinations = []
+	allpositive = copy.deepcopy(hiddenvariables)
+	allnegative = copy.deepcopy(hiddenvariables)
+	combination_list = []
+	number = 0
+	flag = True
+	for i in range(len(allpositive)):
+		allpositive[i] = "+" + allpositive[i]
+	for i in range(len(allnegative)):
+		allnegative[i] = "-" + allnegative[i]
+
+
+	for i in range(2**(len(hiddenvariables) - 1)):
+		number = math.ceil(math.sqrt(i))
+		if number == 0:
+			number = 1
+		combination_list = []
+		for j in range(len(hiddenvariables)):
+
+			if j == number - 1:
+				if flag:
+					combination_list.append(allpositive[j])
+					flag = False
+				else: 
+					combination_list.append(allnegative[j])
+					flag = True
+			else:
+				combination_list.append(allpositive[j])
+
+		combinations.append(combination_list)
+	reverse_combinations = reverse(combinations)
+	for element in reverse_combinations:
+		combinations.append(element)
+	#print(reverse_combinations)
+
+	return combinations
+
+def reverse(combi):
+
+	rev = []
+	aux_list = []
+	for element in combi:
+		aux_list =[]
+		for item in element:
+			if "+" in item:
+				aux_list.append(item.replace("+", "-"))
+			elif "-" in item:
+				aux_list.append(item.replace("-", "+"))
+		rev.append(aux_list)
+	return rev
 
 def chain_rule():
 	#get permutations
@@ -137,7 +271,7 @@ def bayes(nodes, probabilities, queries):
 	nodes_list = create_nodes(nodes)
 	probabilities_list = parse_probabilities(probabilities)
 	set_parents(nodes_list, probabilities_list)
-	parse_query(queries)
+	parse_query(queries, nodes_list)
 	#print(nodes_list)
 	return 0
 
